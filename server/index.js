8ee33aac5e3a06e8296cff5b4c031adf7ef02e48
Nabeel Akbar <studiosnabeel@gmail.com>
2023-08-15 01:17:19 +0500

    connect Mongodb to server

--- server/index.js
+++ server/index.js
@@ -1,8 +1,21 @@
 import express from "express";
 import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const app = express();
 
+const connect = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO);
+    console.log("MongoDB connected!");
+  } catch (err) {
+    throw err;
+  }
+};
+
 app.listen("5000", () => {
+  connect();
   console.log("Server running successfully on port 5000");
 });
