b05ee9304991df48642640732129625034abb610
Nabeel Akbar <studiosnabeel@gmail.com>
2023-08-17 13:46:41 +0500

    Implement Update User method

---
+++ server/controllers/user.js
@@ -0,0 +1,35 @@
+import { createError } from "../error.js";
+import User from "../models/User.js";
+
+export const updateUser = async (req, res, next) => {
+  if (req.params.id === req.user.id) {
+    try {
+      const updatedUser = await User.findByIdAndUpdate(
+        req.params.id,
+        {
+          $set: req.body,
+        },
+        {
+          new: true,
+        }
+      );
+      res.status(200).json(updatedUser);
+    } catch (err) {
+      next(err);
+    }
+  } else {
+    return next(createError(400, "You can only update you own Account!"));
+  }
+};
+
+export const deleteUser = async (req, res, next) => {};
+
+export const getUser = async (req, res, next) => {};
+
+export const subscribe = async (req, res, next) => {};
+
+export const unsubscribe = async (req, res, next) => {};
+
+export const like = async (req, res, next) => {};
+
+export const dislike = async (req, res, next) => {};
--- server/routes/users.js
+++ server/routes/users.js
@@ -1,5 +1,43 @@
 import express from "express";
+import {
+  deleteUser,
+  dislike,
+  getUser,
+  like,
+  subscribe,
+  unsubscribe,
+  updateUser,
+} from "../controllers/user.js";
+import { verifyToken } from "../verifyToken.js";
 
 const router = express.Router();
+
+// Update User
+
+router.put("/:id", verifyToken, updateUser);
+
+// Delete User
+
+router.delete("/:id", deleteUser);
+
+// Get a User
+
+router.get("/find/:id", getUser);
+
+// Subscribe a User
+
+router.put("/sub/:id", subscribe);
+
+// Unsubscribe a User
+
+router.put("/unsub/:id", unsubscribe);
+
+// Like a Video
+
+router.put("/like/:videoId", like);
+
+// Unlike a Video
+
+router.put("/dislike/:videoId", dislike);
 
 export default router;
