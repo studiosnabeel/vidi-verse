6f85ad66cb43c828568c0f20da8840485b8ed6d6
Nabeel Akbar <studiosnabeel@gmail.com>
2023-08-18 07:15:04 +0500

    Create routes for the videos

---
+++ server/controllers/video.js
@@ -0,0 +1,15 @@
+export const addVideo = async (req, res, next) => {
+  //logic
+};
+
+export const updateVideo = async (req, res, next) => {
+  //logic
+};
+
+export const deleteVideo = async (req, res, next) => {
+  //logic
+};
+
+export const getVideo = async (req, res, next) => {
+  //logic
+};
--- server/routes/videos.js
+++ server/routes/videos.js
@@ -1,5 +1,24 @@
 import express from "express";
+import { verifyToken } from "../verifyToken";
+import {
+  addVideo,
+  deleteVideo,
+  getVideo,
+  updateVideo,
+} from "../controllers/video";
 
 const router = express.Router();
+
+//Create a video
+router.post("/", verifyToken, addVideo);
+
+//Update a video
+router.put("/:id", verifyToken, updateVideo);
+
+//Delete a video
+router.delete("/:id", verifyToken, deleteVideo);
+
+//Get a video
+router.get("/find/:id", getVideo);
 
 export default router;
