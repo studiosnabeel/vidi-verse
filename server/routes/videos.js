5b58afa1da686658faeaaaef8adc2c13793d9077
Nabeel Akbar <studiosnabeel@gmail.com>
2023-08-18 09:33:21 +0500

    Add all routes for the videos

--- server/routes/videos.js
+++ server/routes/videos.js
@@ -1,11 +1,11 @@
 import express from "express";
-import { verifyToken } from "../verifyToken";
+import { verifyToken } from "../verifyToken.js";
 import {
   addVideo,
   deleteVideo,
   getVideo,
   updateVideo,
-} from "../controllers/video";
+} from "../controllers/video.js";
 
 const router = express.Router();
 
@@ -20,5 +20,23 @@ router.delete("/:id", verifyToken, deleteVideo);
 
 //Get a video
 router.get("/find/:id", getVideo);
+
+//Add View for video
+router.put("/view/:id");
+
+//Search trending videos
+router.get("/trend");
+
+//Display Random Videos
+router.get("/random");
+
+//subscribe to a video
+router.get("/sub", verifyToken);
+
+// Search by tags
+router.get("/tags");
+
+//Search by search filter
+router.get("/search");
 
 export default router;
