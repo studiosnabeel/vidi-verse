08ad42b7d2bcec8b62eb0db90203d2596e7170e0
Nabeel Akbar <studiosnabeel@gmail.com>
2023-08-18 09:33:11 +0500

    Add update delete video controller methods

--- server/controllers/video.js
+++ server/controllers/video.js
@@ -1,15 +1,57 @@
+import { createError } from "../error.js";
+import Video from "../models/Video.js";
+
 export const addVideo = async (req, res, next) => {
-  //logic
+  const newVideo = new Video({ userId: req.user.id, ...req.body });
+  try {
+    const savedVideo = await newVideo.save();
+    res.status(200).json(savedVideo);
+  } catch (err) {
+    next(err);
+  }
 };
 
 export const updateVideo = async (req, res, next) => {
-  //logic
+  try {
+    const video = await Video.findById(req.params.id);
+    if (!video) return next(createError(500, "Video not found!"));
+    if (req.user.id === video.userId) {
+      const updatedVideo = await Video.findByIdAndUpdate(
+        req.params.id,
+        {
+          $set: req.body,
+        },
+        {
+          new: true,
+        }
+      );
+      res.status(200).json(updatedVideo);
+    } else {
+      return next(createError(400, "You can only update your own video!"));
+    }
+  } catch (err) {
+    next(err);
+  }
 };
 
 export const deleteVideo = async (req, res, next) => {
-  //logic
+  try {
+    const video = await Video.findById(req.params.id);
+    if (!video) return next(createError(400, "Video not Found!"));
+    if (req.user.id === video.userId) {
+      await Video.findByIdAndDelete(req.params.id);
+      res.status(200).json("video deleted successfully!");
+    }
+  } catch (err) {
+    next(err);
+  }
 };
 
 export const getVideo = async (req, res, next) => {
-  //logic
+  try {
+    const video = await video.findById(req.params.id);
+    res.status(200).json(video);
+  } catch (err) {
+    next(err);
+  }
 };
