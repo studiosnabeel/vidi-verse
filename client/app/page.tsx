b6e416679170d3556bc047f38a38891db7979796
Nabeel Akbar <studiosnabeel@gmail.com>
2023-08-23 17:26:03 +0500

    Add prop to card

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -2,57 +2,27 @@ import Card from "@/components/Card";
 
 export default function Home() {
   return (
-    <main className='p-4 w-[100%] flex flex-wrap flex-[7] justify-between gap-4 bg-[#fffef4]'>
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
+    <main className='p-4 w-[100%] flex flex-wrap flex-[7] justify-center gap-4 bg-yellow-50'>
+      <Card isSideBar={false} />
+      <Card isSideBar={false} />
+      <Card isSideBar={false} />
+      <Card isSideBar={false} />
+      <Card isSideBar={false} />
+      <Card isSideBar={false} />
+      <Card isSideBar={false} />
+      <Card isSideBar={false} />
+      <Card isSideBar={false} />
+      <Card isSideBar={false} />
+      <Card isSideBar={false} />
+      <Card isSideBar={false} />
+      <Card isSideBar={false} />
+      <Card isSideBar={false} />
+      <Card isSideBar={false} />
+      <Card isSideBar={false} />
+      <Card isSideBar={false} />
+      <Card isSideBar={false} />
+      <Card isSideBar={false} />
+      <Card isSideBar={false} />
     </main>
   );
 }
