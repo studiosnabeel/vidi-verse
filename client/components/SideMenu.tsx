9e2b70b845e519a451f121a54bed9394f617ac2c
Nabeel Akbar <studiosnabeel@gmail.com>
2023-08-23 16:02:22 +0500

    more style to side bar

--- client/components/SideMenu.tsx
+++ client/components/SideMenu.tsx
@@ -21,24 +21,24 @@ const SideMenu = () => {
       {/* Items */}
 
       <div className='flex flex-col gap-3 justify-center items-start px-2 font-semibold'>
-        <div className='flex gap-2 cursor-pointer'>
+        <div className='flex gap-2 cursor-pointer hover:bg-gray-200 w-full'>
           <VscHome />
-          <h4 className='textsm'>Home</h4>
+          <h4 className='textsm '>Home</h4>
         </div>
-        <div className='flex gap-2 cursor-pointer'>
+        <div className='flex gap-2 cursor-pointer hover:bg-gray-200 w-full'>
           <MdTravelExplore />
           <h4 className='text-sm'>Explore</h4>
         </div>
-        <div className='flex gap-2 cursor-pointer'>
+        <div className='flex gap-2 cursor-pointer hover:bg-gray-200 w-full'>
           <MdOutlineSubscriptions />
           <h4 className='text-sm'>Subscriptions</h4>
         </div>
         <hr className='border-t-1 w-[90%] border-gray-400 my-2' />
-        <div className='flex gap-2 cursor-pointer'>
+        <div className='flex gap-2 cursor-pointer hover:bg-gray-200 w-full'>
           <MdPhotoLibrary />
           <h4 className='text-sm'>Library</h4>
         </div>
-        <div className='flex gap-2 cursor-pointer'>
+        <div className='flex gap-2 cursor-pointer hover:bg-gray-200 w-full'>
           <AiOutlineHistory />
           <h4 className='text-sm'>History</h4>
         </div>
@@ -57,40 +57,40 @@ const SideMenu = () => {
         </div>
         <hr className='border-t-1 w-[90%] border-gray-400 ' />
         <h4 className='text-sm'>Best of VidiVerse</h4>
-        <div className='flex gap-2 cursor-pointer'>
+        <div className='flex gap-2 cursor-pointer hover:bg-gray-200 w-full'>
           <PiMusicNotesSimpleThin />
           <h4 className='text-sm'>Music</h4>
         </div>
-        <div className='flex gap-2 cursor-pointer'>
+        <div className='flex gap-2 cursor-pointer hover:bg-gray-200 w-full'>
           <MdOutlineSportsSoccer />
           <h4 className='text-sm'>Sports</h4>
         </div>
-        <div className='flex gap-2 cursor-pointer'>
+        <div className='flex gap-2 cursor-pointer hover:bg-gray-200 w-full'>
           <SiYoutubegaming />
           <h4 className='text-sm'>Gaming</h4>
         </div>
-        <div className='flex gap-2 cursor-pointer'>
+        <div className='flex gap-2 cursor-pointer hover:bg-gray-200 w-full'>
           <BiCameraMovie />
           <h4 className='text-sm'>Movies</h4>
         </div>
-        <div className='flex gap-2 cursor-pointer'>
+        <div className='flex gap-2 cursor-pointer hover:bg-gray-200 w-full'>
           <PiNewspaperClipping />
           <h4 className='text-sm'>News</h4>
         </div>{" "}
-        <div className='flex gap-2 cursor-pointer'>
+        <div className='flex gap-2 cursor-pointer hover:bg-gray-200 w-full'>
           <MdOutlineLiveTv />
           <h4 className='text-sm'>Live</h4>
         </div>
         <hr className='border-t-1 w-[90%] border-gray-400 mt-2' />
-        <div className='flex gap-2 cursor-pointer'>
+        <div className='flex gap-2 cursor-pointer hover:bg-gray-200 w-full'>
           <MdSettingsApplications />
           <h4 className='text-sm'>Settings</h4>
         </div>{" "}
-        <div className='flex gap-2 cursor-pointer'>
+        <div className='flex gap-2 cursor-pointer hover:bg-gray-200 w-full'>
           <MdOutlineReportProblem />
           <h4 className='text-sm'>Report</h4>
         </div>{" "}
-        <div className='flex gap-2 cursor-pointer'>
+        <div className='flex gap-2 cursor-pointer hover:bg-gray-200 w-full'>
           <MdHelpOutline />
           <h4 className='text-sm'>Help</h4>
         </div>
