7b677c8a11ad4d300fbc836891ddf0384914a852
Nabeel Akbar <studiosnabeel@gmail.com>
2023-08-25 08:42:07 +0500

    Add CORS

--- server/index.js
+++ server/index.js
@@ -6,6 +6,7 @@ import userRoutes from "./routes/users.js";
 import videoRoutes from "./routes/videos.js";
 import commentRoutes from "./routes/comments.js";
 import cookieParser from "cookie-parser";
+import cors from "cors";
 
 dotenv.config();
 
@@ -23,6 +24,7 @@ const connect = async () => {
 // Middlewares
 app.use(cookieParser());
 app.use(express.json());
+app.use(cors());
 app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/videos", videoRoutes);
