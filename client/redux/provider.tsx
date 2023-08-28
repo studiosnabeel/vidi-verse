847cfc6fe7d39923cdf595025f349b3269b20fc1
Nabeel Akbar <studiosnabeel@gmail.com>
2023-08-28 17:59:11 +0500

    Setup redux-persist library

--- client/redux/provider.tsx
+++ client/redux/provider.tsx
@@ -1,12 +1,19 @@
 "use client";
 import { ReactNode, Context } from "react";
+import { PersistGate } from "redux-persist/integration/react";
 import { Provider } from "react-redux";
-import { store, RootState, AppDispatch } from "./store"; // Make sure to adjust the import path for RootState and AppDispatch
+import { store, RootState, AppDispatch, persistor } from "./store"; // Make sure to adjust the import path for RootState and AppDispatch
 
 interface ProvidersProps {
   children: ReactNode;
 }
 
 export const Providers: React.FC<ProvidersProps> = ({ children }) => {
-  return <Provider store={store}>{children}</Provider>;
+  return (
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        {children}
+      </PersistGate>
+    </Provider>
+  );
 };
--- client/redux/store.ts
+++ client/redux/store.ts
@@ -1,12 +1,41 @@
 "use client";
-import { configureStore } from "@reduxjs/toolkit";
+import { configureStore, combineReducers } from "@reduxjs/toolkit";
 import userReducer from "@/redux/userSlice";
+import videoReducer from "@/redux/videoSlice";
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from "redux-persist";
+import storage from "redux-persist/lib/storage";
+import { PersistGate } from "redux-persist/integration/react";
+
+const persistConfig = {
+  key: "root",
+  version: 1,
+  storage,
+};
+
+const rootReducer = combineReducers({ user: userReducer, video: videoReducer });
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 export const store = configureStore({
-  reducer: {
-    user: userReducer,
-  },
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
 });
+
+export const persistor = persistStore(store);
 
 // Infer the `RootState` and `AppDispatch` types from the store itself
 export type RootState = ReturnType<typeof store.getState>;
