454b881a7bfbacef75437b792c4c7ffed86f4d9e
Nabeel Akbar <studiosnabeel@gmail.com>
2023-09-03 14:24:37 +0500

    Add structure and style to Navbar

---
+++ client/src/components/Navbar.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import { FaSearch } from "react-icons/fa";
+import { CgProfile } from "react-icons/cg";
+
+const Navbar = () => {
+  return (
+    <section className=' bg-[#17141b] px-4 py-4 sticky top-0 flex items-center justify-between'>
+      <div className='flex gap-4 items-center justify-between border-2 rounded-full px-4 py-1 w-1/3'>
+        <input
+          className='outline-none bg-[inherit] text-white text-sm w-full'
+          type='text'
+          placeholder='Search Videos'
+        />
+        <FaSearch className='text-white' />
+      </div>
+      <div>
+        <div className='flex gap-2 items-center border-2 border-blue-500 justify-center py-1 w-fit px-2 text-white'>
+          <CgProfile />
+          <h2 className='cursor-pointer'>SIGN IN</h2>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Navbar;
